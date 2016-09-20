ff33c0e75037181aa55dcc4a9231db0f1d924423
Tino Rusch <tino.rusch@webvariants.de>
2016-09-20 14:15:35 +0200

    added license headers;

--- include/susi/MQTTClient.h
+++ include/susi/MQTTClient.h
@@ -1,23 +1,34 @@
+/*
+ * Copyright (c) 2016, Tino Rusch
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author: Tino Rusch (tino.rusch@webvariants.de)
+ */
+
 #include "susi/SusiClient.h"
 #include <mosquittopp.h>
 
 namespace Susi {
-	class MQTTClient : public mosqpp::mosquittopp {
+class MQTTClient : public mosqpp::mosquittopp {
 
-	public:
-		MQTTClient(Susi::SusiClient & susi, BSON::Value & config);
-		void join();
+public:
+  MQTTClient(Susi::SusiClient &susi, BSON::Value &config);
+  void join();
 
-	protected:
-		Susi::SusiClient & susi_;
-		std::thread runloop_;
-		std::vector<std::string> subscriptions_;
+protected:
+  Susi::SusiClient &susi_;
+  std::thread runloop_;
+  std::vector<std::string> subscriptions_;
 
-		void subscribe(const std::string & topic);
-		void forward(const std::string & topic);
+  void subscribe(const std::string &topic);
+  void forward(const std::string &topic);
 
-		void on_connect(int rc);
-		void on_message(const struct mosquitto_message *message);
-		void on_subscribe(int mid, int qos_count, const int *granted_qos);
-	};
+  void on_connect(int rc);
+  void on_message(const struct mosquitto_message *message);
+  void on_subscribe(int mid, int qos_count, const int *granted_qos);
+};
 }
--- main.cpp
+++ main.cpp
@@ -1,25 +1,37 @@
+/*
+ * Copyright (c) 2016, Tino Rusch
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author: Tino Rusch (tino.rusch@webvariants.de)
+ */
+
 #include "susi/MQTTClient.h"
 #include "susi/BaseApp.h"
 
 class MQTTApp : public Susi::BaseApp {
 protected:
-    std::shared_ptr<Susi::MQTTClient> _mqttComponent;
+  std::shared_ptr<Susi::MQTTClient> _mqttComponent;
+
 public:
-    MQTTApp(int argc, char **argv) : Susi::BaseApp{argc,argv} {}
-    virtual ~MQTTApp() {}
-    virtual void start() override {
-        _mqttComponent.reset(new Susi::MQTTClient{*_susi,_config["component"]});
-    }
+  MQTTApp(int argc, char **argv) : Susi::BaseApp{argc, argv} {}
+  virtual ~MQTTApp() {}
+  virtual void start() override {
+    _mqttComponent.reset(new Susi::MQTTClient{*_susi, _config["component"]});
+  }
 };
 
-int main(int argc, char *argv[]){
-    try{
-        MQTTApp app{argc,argv};
-        app.start();
-        app.join();
-    }catch(const std::exception & e){
-        std::cout << e.what() << std::endl;
-        return 1;
-    }
-    return 0;
+int main(int argc, char *argv[]) {
+  try {
+    MQTTApp app{argc, argv};
+    app.start();
+    app.join();
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
 }
